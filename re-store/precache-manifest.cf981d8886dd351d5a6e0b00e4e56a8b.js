82aca3bde4312ce00d73fb690e046cdbd7d498ee
kaigor619 <kaigor619@gmail.com>
2019-07-23 02:54:26 +0300

    re-store 3

---
+++ re-store/precache-manifest.cf981d8886dd351d5a6e0b00e4e56a8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd18008c089590cefc41a72df4038b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e44509ced2e7dbf07ae",
+    "url": "/static/css/main.92e250a3.chunk.css"
+  },
+  {
+    "revision": "80b0c8faefcfeac5dd60",
+    "url": "/static/js/2.b718aaed.chunk.js"
+  },
+  {
+    "revision": "9e44509ced2e7dbf07ae",
+    "url": "/static/js/main.0a88112d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2d35a47a5fefe61d2e9c428dac051d6b",
+    "url": "/static/media/error_image.2d35a47a.jpg"
+  }
+]);
