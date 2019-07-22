1e003d0e7c83792f0177a1415db43d07e36e11f3
kaigor619 <kaigor619@gmail.com>
2019-07-23 02:10:17 +0300

    todo

---
+++ todo/precache-manifest.9aaa89dd6d0cb3c700f5e8eb0c58c22e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2918d732d3be64dde892878ec9de2695",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba946bb66b8559c85e5f",
+    "url": "/static/js/2.0975ab2f.chunk.js"
+  },
+  {
+    "revision": "fd08a707b1c18497264a",
+    "url": "/static/js/main.b23a9125.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
