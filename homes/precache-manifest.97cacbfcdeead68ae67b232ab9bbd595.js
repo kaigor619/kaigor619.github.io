c29f699bf19f842670908d3c61b956876a4486ff
kaigor619 <kaigor619@gmail.com>
2019-08-16 13:23:54 +0300

    homes

---
+++ homes/precache-manifest.97cacbfcdeead68ae67b232ab9bbd595.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c7c2f1f72530d87e83ff5d69d3e48b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28189a7af67bfb11eb78",
+    "url": "/static/css/main.2463e002.chunk.css"
+  },
+  {
+    "revision": "6c691afaec2f387e0abb",
+    "url": "/static/js/2.607e3502.chunk.js"
+  },
+  {
+    "revision": "28189a7af67bfb11eb78",
+    "url": "/static/js/main.98522e35.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
