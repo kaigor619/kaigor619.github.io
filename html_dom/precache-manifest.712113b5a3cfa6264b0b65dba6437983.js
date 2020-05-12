976506d87bedb0b3dd262bc755f42fb39e49c7bb
kaigor619 <38076486+kaigor619@users.noreply.github.com>
2020-05-13 00:25:09 +0300

    add html_dom project

---
+++ html_dom/precache-manifest.712113b5a3cfa6264b0b65dba6437983.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccd46d58e20ac468886199280128141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee87f21f959d8aaa4553",
+    "url": "/static/css/main.0990ce80.chunk.css"
+  },
+  {
+    "revision": "d51f710b9a79e9059ccf",
+    "url": "/static/js/2.3639c864.chunk.js"
+  },
+  {
+    "revision": "0d49efbd01944beaf400240e79598b69",
+    "url": "/static/js/2.3639c864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee87f21f959d8aaa4553",
+    "url": "/static/js/main.727b5abc.chunk.js"
+  },
+  {
+    "revision": "1ee0d1fa24904bc540bb",
+    "url": "/static/js/runtime-main.268ba3ec.js"
+  },
+  {
+    "revision": "1477d7bd825f81f36a4912dc3d17bc0e",
+    "url": "/static/media/arrow_dom.1477d7bd.svg"
+  },
+  {
+    "revision": "a81d18ab4fe0ec7b32bb19f25008bd3f",
+    "url": "/static/media/close.a81d18ab.svg"
+  },
+  {
+    "revision": "b9a1b03b7bf2dc72b9ef4d301f40e622",
+    "url": "/static/media/inherit_unvisible.b9a1b03b.svg"
+  },
+  {
+    "revision": "ea03a2acb7012e0920dfb4c9368eaf45",
+    "url": "/static/media/layer.ea03a2ac.svg"
+  },
+  {
+    "revision": "22a31ebe46c03aafbd24b116e299bcf5",
+    "url": "/static/media/not_eye.22a31ebe.svg"
+  },
+  {
+    "revision": "c195f278188c774da8bb940cac25b99d",
+    "url": "/static/media/view.c195f278.svg"
+  }
+]);
